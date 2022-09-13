3c1f26cf557b946fae2f7bd656c3d3e72588cf7f
NeMoMinik <104688335+NeMoMinik@users.noreply.github.com>
2022-09-13 12:35:40 +0300

    update1
    something smart and useful

--- test_world_download.sh
+++ test_world_download.sh
@@ -5,7 +5,7 @@ cd educational_worlds/
 cp -rf models/ /home/clover/.gazebo/
 cp -f simulator.launch /home/clover/catkin_ws/src/clover/clover_simulation/launch/
 cp -f parquet.jpg /home/clover/catkin_ws/src/clover/clover_simulation/models/parquet_plane/materials/textures/
-cp -f shirokaya_1.world /home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/
+cp -f amogus.world /home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/
 cd ..
 rm -rf educational_worlds/
 echo "Done!"
