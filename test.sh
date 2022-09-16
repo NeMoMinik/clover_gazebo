7e317b59171f2e7150fbc00771779658db3001b7
NeMoMinik <104688335+NeMoMinik@users.noreply.github.com>
2022-09-16 11:50:02 +0300

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#gittest
+git clonehttps://github.com/NeMoMinik/clover_gazebo.git
+cd clover_gazebo/
+cp -rf models/ /home/clover/.gazebo/
+cp -f simulator.launch /home/clover/catkin_ws/src/clover/clover_simulation/launch/
+cp -f parquet.jpg /home/clover/catkin_ws/src/clover/clover_simulation/models/parquet_plane/materials/textures/
+cp -f amogus.world /home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/
+cp -f spawn_drone.launch /home/clover/catkin_ws/src/clover/clover_description/launch/
+cd ..
+rm -rf educational_worlds/
+echo "Done!"
